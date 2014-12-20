233d57e9bc099fc867c6bed3e7baae7130d33ad9
Greyson Fischer <greyson@foosoft.us>
2014-12-19 23:57:35 -0500

    All done with pretty showing.
    I rather like the format -- but then I use a terminal wider than 80 chars.

--- Minecraft/NBT.hs
+++ Minecraft/NBT.hs
@@ -2,22 +2,21 @@
 module Minecraft.NBT where
 
 import Control.Applicative ( (<$>) )
+import Control.Monad (replicateM)
 import Data.Binary (Binary(..))
-import Data.Binary.Get (Get(..), runGet, runGetOrFail, ByteOffset,
+import Data.Binary.Get (Get(..),
                         getWord8, getWord16le, getWord32le, getWord64le, getByteString)
-import Data.Binary.Put (Put(..), runPut,
+import Data.Binary.Put (Put(..),
                         putWord8, putWord16le, putWord32le, putWord64le, putByteString)
-import Data.ByteString.Lazy (ByteString, toStrict, fromStrict)
-import qualified Data.ByteString.Lazy.UTF8 as UTF8
-import qualified Data.ByteString.Lazy as BL
+import Data.ByteString.Lazy (ByteString, fromStrict)
 import Data.Int (Int8, Int16, Int32, Int64)
 import Data.Map (Map(..))
-import Data.ReinterpretCast
+import Data.ReinterpretCast (wordToFloat, wordToDouble)
 import Data.Text.Encoding (encodeUtf8, decodeUtf8)
-import Data.Typeable (cast)
-import Data.Word (Word8, Word16, Word32, Word64)
+import Text.Printf (printf)
 
 import qualified Data.Text as T
+import qualified Data.ByteString.Lazy as BL
 
 data TagType = EndType
              | ByteType
@@ -38,7 +37,7 @@ instance Binary TagType where
   put = putWord8 . fromIntegral . fromEnum
 
 data NBT = NBT T.Text NBTPayload
-         deriving (Show, Eq)
+         deriving (Eq)
 
 data NBTPayload = EndTag
                 | ByteTag Int8
@@ -64,12 +63,11 @@ getByType DoubleType = DoubleTag . wordToDouble <$> getWord64le
 getByType ByteArrayType = do
   len <- fromIntegral <$> getWord32le
   ByteArrayTag . fromStrict <$> getByteString len
-getByType StringType = do
-  StringTag <$> getStringValue
+getByType StringType = StringTag <$> getStringValue
 getByType ListType = do
   typ <- get
   len <- fromIntegral <$> getWord32le
-  ListTag typ <$> (sequence $ take len $ repeat (getByType typ))
+  ListTag typ <$> replicateM len (getByType typ)
 getByType CompoundType = CompoundTag <$> getToEnd
   where
     getToEnd = do
@@ -79,11 +77,10 @@ getByType CompoundType = CompoundTag <$> getToEnd
         else do
         name <- getStringValue
         payload <- getByType typ
-        ((NBT name payload):) <$> getToEnd
+        (NBT name payload:) <$> getToEnd
 getByType IntArrayType = do
   len <- fromIntegral <$> getWord32le
-  IntArrayTag <$> (sequence $ take len $
-                   repeat (fromIntegral <$> getWord32le))
+  IntArrayTag <$> replicateM len (fromIntegral <$> getWord32le)
 
 getStringValue :: Get T.Text
 getStringValue = do
@@ -98,220 +95,91 @@ instance Binary NBT where
     payload <- getByType CompoundType
     return $ NBT name payload
 
-{-
- - SECOND DRAFT BELOW
- -
- -    I'm just saving it for the pretty printing later.
- -}
+typeOf :: NBTPayload -> TagType
+typeOf (ByteTag _) = ByteType
+typeOf (ShortTag _) = ShortType
+typeOf (IntTag _) = IntType
+typeOf (LongTag _) = LongType
+typeOf (FloatTag _) = FloatType
+typeOf (DoubleTag _) = DoubleType
+typeOf (ByteArrayTag _) = ByteArrayType
+typeOf (StringTag _) = StringType
+typeOf (ListTag _ _) = ListType
+typeOf (CompoundTag _) = CompoundType
+typeOf (IntArrayTag _) = IntArrayType
 
+instance Show NBT where
+  show = showPretty 0
 
-class Taggable a where
-  tagType :: a -> Word8
-
-class NBTElement a where
-  toNbt :: a -> NBT
-
-data NBT' = NBTEnd
-         | NBTByte Int8
-         | NBTShort Int16
-         | NBTInt Int32
-         | NBTLong Int64
-         | NBTFloat Float
-         | NBTDouble Double
-         | NBTByteArray ByteString
-         | NBTString String
-         | NBTList [NBT']
-         | NBTCompound [NBTNamed]
-         | NBTIntList [Int32]
-
-newtype NBTNamed = NBTNamed (String, NBT')
-
-instance Taggable () where
-  tagType _ = 0
-instance Taggable Int8 where
-  tagType _ = 1
-instance Taggable Int16 where
-  tagType _ = 2
-instance Taggable Int32 where
-  tagType _ = 3
-instance Taggable Int64 where
-  tagType _ = 4
-instance Taggable Float where
-  tagType _ = 5
-instance Taggable Double where
-  tagType _ = 6
-instance Taggable ByteString where
-  tagType _ = 7
-instance Taggable String where
-  tagType _ = 8
-instance Taggable a => Taggable [a] where
-  tagType _ = 9
-instance Taggable [NBTNamed] where
-  tagType _ = 10
-
-
-getTag :: Taggable a => a -> Word8
-getTag a = tagType a
-
-
-class PrettyPrintable a where
+class Show a => PrettyPrintable a where
   showPretty :: Int -> a -> String
+  showPretty x v = indent x ++ showUnindented v ++ "\n"
+
+  showUnindented :: a -> String
+  showUnindented = show
+
+instance PrettyPrintable NBT where
+  showPretty ind (NBT name payload) =
+    indent ind ++ show (typeOf payload)
+    ++ "(" ++ show name ++ "): "
+    ++ showPretty ind payload
+
+instance PrettyPrintable NBTPayload where
+  showPretty ind (CompoundTag l) =
+    showEntryCount (length l) ++ "\n"
+    ++ concatMap (showPretty (succ ind)) l
+
+  showPretty _ (ByteTag b) = show b ++ "\n"
+  showPretty _ (ShortTag s) = show s ++ "\n"
+  showPretty _ (IntTag i) = show i ++ "\n"
+  showPretty _ (LongTag l) = show l ++ "\n"
+  showPretty _ (FloatTag f) = show f ++ "\n"
+  showPretty _ (DoubleTag d) = show d ++ "\n"
+
+  showPretty ind (ByteArrayTag b) =
+    '\n' : (unlines $ map showBareLine (binGroupBy 40 b))
+    where
+      showBareLine l = indent (succ ind) ++ showGroups (binGroupBy 2 l)
+      showGroups g = unwords $ map prettyPrintBinary g
+
+  showPretty _ (StringTag s) = show s ++ "\n"
+
+  -- Treat lists of compound objects a bit differently.
+  showPretty ind (ListTag CompoundType l) =
+    showEntryCount (length l) ++ " [" ++ show CompoundType ++ "]\n"
+    ++ concatMap showBareCompound l
+    where
+      showBareCompound c =
+        indent (succ ind) ++ "{}: "
+        ++ showPretty (succ ind) c
+
+  -- Also lists of lists
+
+  showPretty ind (ListTag t l) =
+    showEntryCount (length l) ++ " [" ++ show t ++ "]\n"
+    ++ showPretty (succ ind) l
+
+
+  showPretty x v = indent x ++ show v ++ "\n"
+
+instance PrettyPrintable a => PrettyPrintable [a] where
+  showPretty ind l = indent ind ++ show l ++ "\n"
+
+indent x = replicate (3 * x) ' '
 
 showEntryCount 1 = "1 entry"
 showEntryCount n = show n ++ " entries"
 
-showIndent n = take (3 * n) $ cycle " "
+binGroupBy n l
+  | BL.null l = []
+  | otherwise =
+    let (line, rest) = BL.splitAt n l
+    in line:binGroupBy n rest
 
-instance Show NBT' where
-  show (NBTByte i) = show i
-  show (NBTShort s) = show s
-  show (NBTInt a) = show a
-  show (NBTString s) = show s
-  show x = showPretty 0 x
-
-instance PrettyPrintable NBT' where
-  showPretty indent (NBTCompound l) =
-    let showNext x = showPretty (succ indent) x ++ "\n"
-    in showEntryCount (length l) ++ "\n"
-       ++ showIndent indent ++ "{\n"
-       ++ concat (map showNext l)
-       ++ showIndent indent ++ "}"
-
-  showPretty indent (NBTList l) =
-    let showElem x =
-          tagName x ++ "(): "
-          ++ showPretty (succ indent) x ++ "\n"
-    in showEntryCount (length l) ++ "\n"
-       ++ showIndent indent ++ "[\n"
-       ++ concat (map showElem l)
-       ++ showIndent indent ++ "]"
-  showPretty indent other =
-    showIndent indent ++ show other
-
-instance Show NBTNamed where
-  show (NBTNamed (name, nbt)) = tagName nbt ++ "(" ++ show name ++ "): " ++ show nbt
-
-instance PrettyPrintable NBTNamed where
-  showPretty indent other = showIndent indent ++ show other
-
-tagName (NBTByte _) = "TAG_Byte"
-tagName (NBTShort _) = "TAG_Short"
-tagName (NBTInt _) = "TAG_Int"
-tagName (NBTLong _) = "TAG_Long"
-tagName (NBTString _) = "TAG_String"
-tagName (NBTList _) = "TAG_List"
-tagName (NBTCompound _) = "TAG_Compound"
-
-
-{-
-tagType (NBTByte _) = 1
-tagType (NBTShort _) = 2
-tagType (NBTInt _) = 3
-tagType (NBTLong _) = 4
-tagType (NBTFloat _) = 5
-tagType (NBTDouble _) = 6
-tagType (NBTByteArray _) = 7
-tagType (NBTString _) = 8
-tagType (NBTCompound _) = 9
-
-instance Binary Tag where
-  get = undefined
-  put (NBTByte b) = putWord8 $ fromIntegral b
-  put (NBTShort s) = putWord16be $ fromIntegral s
-  put (NBTInt i) = putWord32be $ fromIntegral i
-  put (NBTLong l) = putWord64be $ fromIntegral l
-  put (NBTFloat f) = putWord32be (floatToWord f)
-  put (NBTDouble d) = putWord64be (doubleToWord d)
-  put (NBTByteArray a) = do putWord32be $ fromIntegral (length a)
-                            putByteString a
-  put (NBTCompound m) = do
-    putWord8 (tagType (NBTCompound m))
--}
-
-{- | Define types which can act as the payload in NBT values.
- -}
-class Payloadable a where
-  payload :: a -> Put
-  payunload :: Get a
-
-instance Payloadable Int8 where
-  payload = putWord8 . fromIntegral
-  payunload = fromIntegral `fmap` getWord8
-instance Payloadable Int16 where
-  payload = putWord16le . fromIntegral
-  payunload = fromIntegral `fmap` getWord16le
-instance Payloadable Int32 where
-  payload = putWord32le . fromIntegral
-  payunload = fromIntegral `fmap` getWord32le
-instance Payloadable Int64 where
-  payload = putWord64le . fromIntegral
-  payunload = fromIntegral `fmap` getWord64le
-instance Payloadable Float where
-  payload = putWord32le . floatToWord
-  payunload = wordToFloat `fmap` getWord32le
-instance Payloadable Double where
-  payload = putWord64le . doubleToWord
-  payunload = wordToDouble `fmap` getWord64le
-instance Payloadable ByteString where
-  payload a = do
-    putWord32le $ fromIntegral (BL.length a)
-    putByteString $ toStrict a
-  payunload = do
-    bytelen <- fromIntegral `fmap` getWord32le
-    fromStrict `fmap` getByteString bytelen
-instance Payloadable String where
-  payload s = do
-    let bs = UTF8.fromString s
-    putWord16le $ fromIntegral (BL.length bs)
-    putByteString $ toStrict bs
-  payunload = do
-    bytelen <- fromIntegral `fmap` getWord16le
-    getByteString bytelen >>= return . UTF8.toString . fromStrict
-
-newtype NBTFile = NBTFile (String, [NBTNamed])
-
-instance Show NBTFile where
-  show (NBTFile (name, l)) = showPretty 0 (NBTNamed (name, NBTCompound l))
-
-instance Binary NBTFile where
-  put = undefined
-  get = do
-    10 <- getWord8
-    name <- payunload
-    all <- getNBTNamed []
-    -- Parse the rest
-    return $ NBTFile (name, all)
-
-getList :: Get [NBT']
-getList = do
-  t <- getWord8
-  len <- fromIntegral `fmap` (payunload :: Get Int32)
-  value <- sequence $ take len $ repeat (getNBT t)
-  return value
-
-getNBT :: Word8 -> Get NBT'
-getNBT t = case t of
-      1 -> fmap NBTByte payunload
-      2 -> fmap NBTShort payunload
-      3 -> fmap NBTInt payunload
-      4 -> fmap NBTLong payunload
-      5 -> fmap NBTFloat payunload
-      6 -> fmap NBTDouble payunload
-      7 -> fmap NBTByteArray payunload
-      8 -> fmap NBTString payunload
-      9 -> fmap NBTList getList
-      10 -> return $ NBTCompound [] -- TODO: testing
-      11 -> return $ NBTList []
-
-getNBTNamed :: [NBTNamed] -> Get [NBTNamed]
-getNBTNamed prev = do
-  t <- getWord8
-  if t == 0
-    then return prev
-    else do
-    name <- payunload :: Get String
-    value <- getNBT t
-    getNBTNamed (prev ++ [NBTNamed (name, value)])
+prettyPrintBinary :: ByteString -> String
+prettyPrintBinary bin
+  | BL.null bin = []
+  | otherwise = printf "%02x" (BL.head bin) ++ prettyPrintBinary (BL.tail bin)
 
 
 nbtGet :: Get NBT
