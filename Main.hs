4f90088f1a9263b284425c3489b5003db19d07ad
Greyson Fischer <greyson@foosoft.us>
2014-12-21 20:47:33 -0500

    Bad performance? Where?
    Nevermind, bad performance was from repeatedly querying the
    database 16k times just to lookup 128 blocks....

--- Main.hs
+++ Main.hs
@@ -23,11 +23,21 @@ path = "My World/db"
 getPlayer :: IO (Maybe NBT)
 getPlayer = runResourceT $ open path >>= getNbt LocalPlayer
 
-getChunk0Terrain db = runResourceT $ loadChunk 0 0 db
+getChunk0Terrain db = loadChunk 0 0 db
 
-getBlockColumn x z db =
-  sequence $ map (\i -> getBlock x i z <$> terrain <$> getChunk0Terrain db) [0..127]
+getBlockColumn x z ter =
+  map (\y -> getBlock x y z ter) [0..127]
+
+getChunkLayer y ter =
+  map (\(x,z) -> getBlock x y z) $ cartProd [0..15] [1..15]
+  where
+    cartProd xs ys = [(x,y) | x <- xs, y <- ys]
 
 main = runResourceT $ do
   db <- open path
-  length <$> (sequence $ map (\i -> getBlockColumn 0 i db) [0..127])
+  -- remember to load the chunk only once; re-fetching is terribly slow.
+  chunk0terrain <- terrain <$> getChunk0Terrain db
+
+  let columns = map (\i -> getBlockColumn 0 i chunk0terrain) [0..127]
+  liftIO $ print (last columns)
+  liftIO $ putStrLn (show (length columns) ++ " columns")
