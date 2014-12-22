4e470290429cc1ab9362542c03378aeba9ef3de1
Greyson Fischer <greyson@foosoft.us>
2014-12-21 23:10:02 -0500

    Basic ascii output, yay!
    Layer by layer in chunk 0 only; it's worth the time it took now.

--- Main.hs
+++ Main.hs
@@ -14,6 +14,7 @@ import Data.Minecraft.Chunk
 import Data.Minecraft.Block
 
 -- for interactive
+import qualified Data.Map as M
 import qualified Data.Text as T
 
 path = "My World/db"
@@ -29,15 +30,92 @@ getBlockColumn x z ter =
   map (\y -> getBlock x y z ter) [0..127]
 
 getChunkLayer y ter =
-  map (\(x,z) -> getBlock x y z) $ cartProd [0..15] [1..15]
+  map (\(x,z) -> getBlock x y z ter) $ cartProd [0..15] [0..15]
   where
-    cartProd xs ys = [(x,y) | x <- xs, y <- ys]
+    cartProd is js = [(j,i) | i <- is, j <- js]
 
-main = runResourceT $ do
+layerToAscii y ter =
+  splitN 16 $ map blockAscii $ getChunkLayer y ter
+
+splitN n list
+  | null list = []
+  | otherwise =
+    let (line, rest) = splitAt n list
+    in line:splitN n rest
+
+layer n = runResourceT $ do
   db <- open path
   -- remember to load the chunk only once; re-fetching is terribly slow.
   chunk0terrain <- terrain <$> getChunk0Terrain db
 
-  let columns = map (\i -> getBlockColumn 0 i chunk0terrain) [0..127]
-  liftIO $ print (last columns)
-  liftIO $ putStrLn (show (length columns) ++ " columns")
+  liftIO $ putStrLn $ unlines (layerToAscii n chunk0terrain)
+  liftIO $ putStrLn $ unlines (layerToAscii (n+1) chunk0terrain)
+
+blockAscii bt =
+  case M.lookup bt asciiBlock of
+    Nothing -> '?'
+    Just c -> c
+
+asciiBlock = M.fromList [(Air,                ' '), (Stone,              '#'),
+                         (GrassBlock,         '.'), (Dirt,               ','),
+                         (Cobblestone,        '%'), (OakPlank,           '='),
+                         (Sapling,            '!'), (Bedrock,            '&'),
+                         (Water,              '~'), (StationaryWater,    '-'),
+                         (Lava,               'x'), (StationaryLava ,    'X'),
+                         (Sand,               'o'), (Gravel,             ':'),
+                         (GoldOre,            '$'), (IronOre,            '@'),
+                         (CoalOre,            'b'), (Wood,               '|'),
+                         (Leaves,             '^'), (Sponge,             '¶'),
+                         (Glass,              'O'), (LapisOre,           'B'),
+                         (LapisBlock,         '/'), (Sandstone,          '/'),
+                         (Bed,                '/'), (PoweredRail,        '/'),
+                         (Cobweb,             '/'), (TallGrass,          '/'),
+                         (DeadBush,           '/'), (Wool,               '/'),
+                         (Dandelion,          '/'), (Poppy,              '/'),
+                         (BrownMushroom,      '/'), (RedMushroom,        '/'),
+                         (GoldBlock,          '/'), (IronBlock,          '/'),
+                         (DoubleStoneSlab,    '/'), (StoneSlab,          '/'),
+                         (BrickBlock,         '/'), (TNT,                '/'),
+                         (Bookshelf,          '/'), (MossStone,          '/'),
+                         (Obsidian,           '/'), (Torch,              '/'),
+                         (Fire,               '/'), (MonsterSpawner,     '/'),
+                         (OakStairs,          '/'), (Chest,              '/'),
+                         (DiamondOre,         '/'), (DiamondBlock,       '/'),
+                         (CraftingTable,      '/'), (Seeds,              '/'),
+                         (Farmland,           '/'), (Furnace,            '/'),
+                         (BurningFurnace,     '/'), (SignPost,           '/'),
+                         (OakDoor,            '/'), (Ladder,             '/'),
+                         (Rail,               '/'), (CobblestoneStairs,  '/'),
+                         (WallSign,           '/'), (IronDoor,           '/'),
+                         (RedstoneOre,        '/'), (GlowingRedstoneOre, '/'),
+                         (SnowCover,          '/'), (Ice,                '/'),
+                         (Snow,               '/'), (Cactus,             '/'),
+                         (Clay,               '/'), (SugarCane,          '/'),
+                         (Fence,              '/'), (Pumpkin,            '/'),
+                         (Netherrack,         '/'), (Glowstone,          '/'),
+                         (JackOLantern,       '/'), (CakeBlock,          '/'),
+                         (InvisibleBedrock,   '/'), (Trapdoor,           '/'),
+                         (StoneBrick,         '/'), (HugeBrownMushroom,  '/'),
+                         (HugeRedMushroom,    '/'), (IronBars,           '/'),
+                         (GlassPane,          '/'), (Melon,              '/'),
+                         (PumpkinStem,        '/'), (MelonStem,          '/'),
+                         (Vines,              '/'), (FenceGate,          '/'),
+                         (BrickStairs,        '/'), (StoneBrickStairs,   '/'),
+                         (Mycellium,          '/'), (LilyPad,            '/'),
+                         (NetherBrick,        '/'), (NetherBrickStairs,  '/'),
+                         (EndPortalFrame,     '/'), (EndStone,           '/'),
+                         (Cocoa,              '/'), (SandstoneStairs,    '/'),
+                         (EmeraldOre,         '/'), (EmeraldBlock,       '/'),
+                         (SpruceStairs,       '/'), (BirchStairs,        '/'),
+                         (JungleStairs,       '/'), (CobblestoneWall,    '/'),
+                         (Carrots,            '/'), (Potato,             '/'),
+                         (QuartzBlock,        '/'), (QuartzStairs,       '/'),
+                         (OakDoubleSlab,      '/'), (OakSlab,            '/'),
+                         (StainedClay,        '/'), (AcaciaStairs,       '/'),
+                         (DarkOakStairs,      '/'), (HayBlock,           '/'),
+                         (Carpet,             '/'), (HardClay,           '/'),
+                         (CoalBlock,          '/'), (PackedIce,          '/'),
+                         (Podzol,             '/'), (Beetroot,           '/'),
+                         (StoneCutter,        '/'), (GlowingObsidian,    '/'),
+                         (NetherReactor,      '/'), (UpdateGameBE,       '/'),
+                         (UpdateGameLE,       '/'), (NameBlock,          '/')]
