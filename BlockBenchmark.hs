71f4f103c39144e7f2938885ab4aa6e16aa611da
Greyson Fischer <greyson@foosoft.us>
2014-12-30 19:03:18 -0500

    Benchmarking better?

--- BlockBenchmark.hs
+++ BlockBenchmark.hs
@@ -7,8 +7,11 @@ import Data.Minecraft.Block
 decodingN :: Int -> [BlockType]
 decodingN i = map toEnum [0..i]
 
+isUnknown (Unknown _) = True
+isUnknown _ = False
+
 main = defaultMain [
-  bgroup "decode" [ bench "10"  $ whnf decodingN 10
-                  , bench "all" $ whnf decodingN 255
+  bgroup "decode" [ bench "10"  $ whnf (length . (map isUnknown) . decodingN) 10
+                  , bench "all" $ whnf (length . (map isUnknown) . decodingN) 255
                   ]
   ]
