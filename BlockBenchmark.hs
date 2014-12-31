1ae18e3de3f4c02059e95813c6047515ac00661f
Greyson Fischer <greyson@foosoft.us>
2014-12-30 20:00:18 -0500

    Actual benchmark

--- BlockBenchmark.hs
+++ BlockBenchmark.hs
@@ -1,8 +1,10 @@
 module Main where
 
-
+import Control.DeepSeq (NFData(..))
 import Criterion.Main
 import Data.Minecraft.Block
+
+instance NFData BlockType
 
 decodingN :: Int -> [BlockType]
 decodingN i = map toEnum [0..i]
@@ -11,7 +13,7 @@ isUnknown (Unknown _) = True
 isUnknown _ = False
 
 main = defaultMain [
-  bgroup "decode" [ bench "10"  $ whnf (length . (map isUnknown) . decodingN) 10
-                  , bench "all" $ whnf (length . (map isUnknown) . decodingN) 255
+  bgroup "decode" [ bench "10"  $ nf decodingN 10
+                  , bench "all" $ nf decodingN 65535
                   ]
   ]
